7c0f9c2088f71b6aa9e0ff6c5b4c2322cbaac16d
Justin Angel <justinangel86@gmail.com>
2019-04-08 22:26:30 -0400

    changes afoot

--- BruteLoops/examples/__init__.py
+++ BruteLoops/examples/__init__.py
@@ -1,4 +1,53 @@
 #!/usr/bin/env python3
+
+from . import owa2016
+from . import adfs
+from . import fake
+
 from .owa2016 import *
 from .adfs import *
 from .fake import *
+
+from pathlib import Path
+
+from sys import modules,exit
+from pathlib import Path
+from importlib.util import spec_from_file_location,module_from_spec
+import inspect
+from re import match
+
+# =====================================
+# GET THE PATH TO THE MODULES DIRECTORY
+# =====================================
+m = match(r'/.+/',inspect.getfile(
+        inspect.currentframe()
+    )
+)
+
+# ========================
+# DYNAMICALLY LOAD MODULES
+# ========================
+
+# Catch each module in a dictionary to be read by the main program
+handles = {}
+
+base = m.string[m.start():m.end()]
+
+# Sort the file names to organize the modules by name at the main interface
+files = sorted(
+    [
+        f.name for f in Path(base).glob('**/*.py')
+        if not f.name.startswith('_')
+    ]
+)
+
+for f in files:
+
+    mname = f[:len(f)-3]
+
+    # https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
+    # This is pretty much magic to me
+    spec = spec_from_file_location(mname, base+f)
+    mod = module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    handles[mname] = mod
---
+++ example_harness.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from BruteLoops.jitter import Jitter
+from BruteLoops.brute import Horizontal
+from BruteLoops.config import Config
+from BruteLoops.logging import GENERAL_EVENTS,logging
+from BruteLoops.examples import handles
+
+import cmd, sys
+from types import MethodType
+from suffix_printer import *
+
+class Interface(cmd.Cmd):
+    intro = '[Initializing Example Harness 0.01]'
+    prompt = '[BruteLoops]> '
+
+    def __init__(self, *args, **kwargs):
+        self.module = None
+        self._module = None
+        self.attacks = {}
+        super().__init__(*args,**kwargs)
+
+    def do_show(self,target):
+
+        if target == 'modules':
+            print('\n'+'\n'.join(handles.keys())+'\n')
+        elif target == 'jobs':
+            print('SHOWING JOBS')
+        elif target == 'config':
+            if not self.module: 
+                print('NO MODULE SELECTED')
+                return
+            print()
+            for k,v in self._module.config.__dict__.items():
+                if v.__class__ == MethodType: continue
+                print(f'{k}: {v}')
+
+
+    def do_use(self,target):
+
+        if target in handles:
+            self.module = target
+            self._module = handles[target]
+            self._module.__setattr__('config',Config())
+            self.prompt=f'[BruteLoops][{target}]> '
+
+    def do_exit(self,args):
+        sprint('Exiting')
+        sys.exit()
+
+if __name__ == '__main__':
+    Interface().cmdloop()
