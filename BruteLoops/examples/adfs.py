e0d421deecf93fbbf3a982ca9596ae3551436c62
Justin Angel <justinangel86@gmail.com>
2020-08-16 07:15:35 -0600

    updates

--- BruteLoops/examples/adfs.py
+++ BruteLoops/examples/adfs.py
@@ -1,37 +1,26 @@
 from re import search
 import requests
 import warnings
-import urllib
+from urllib.parse import unquote
 warnings.filterwarnings('ignore')
 
 class ADFS():
 
     def __init__(self,url,headers={},proxies={},verify_ssl=False,timeout=5):
-        
-        self.url = url
+        '''
+        url - Should contain the full url, including query string, that will be
+        sent to the server during authentication. I generally use the one generated
+        by Microsoft after being redirected to a federated ADFS server.
+        '''
+        # Unquote the URL to avoid double-quoting the query string
+        self.url = unquote(url)
         self.headers = headers
         self.proxies = proxies
         self.verify_ssl = verify_ssl
         self.timeout = timeout
 
-    def __call__(username,password):
-    
-        # query string
-        query = urllib.parse.unquote('client-request-id=826a908e-c842-488c-8ade-'\
-            '3a95778dd5cc&wa=wsignin1.0&wtrealm=urn%3afederation%3aMicrosoftOnli'\
-            'ne&wctx=LoginOptions%3D3%26estsredirect%3d2%26estsrequest%3drQIIAY2'\
-            'Rv4vTUADHL00vcjeoiDi46CAIQpqXvDS_QDCv7V25pkRbQ69dyiXNS5tr-mLz0pQiCE'\
-            '5yqHRxcdPBoaM4iH-BHA5dXDo4OImTON2mLS5u-h0-fNfv53uLFQuicQP8icRvyAOMR'\
-            'd7zN-2vjC_tXnw-Dx-jj0-rJ1-MF9mzz58WzLU-pXFiCAJJ6ZCQ4wLBeOD5BY9EAsmO'\
-            'hPcMs2SYRU5VoKIpmippxXXRoazrBVAEut7DHi9BHfKyhyGvu1qRF6HoAs3DiuxKq9w'\
-            'F20xpX9qAjAcz_2duB5Nx1I1JQl-yD8vtyUG5bQYVE6GBatbQXmTDtj9Fs8Ts26E13U'\
-            'cJhLDeKcthv2GG9Vm5mVTu1StlFFA7KJbQrOFaWd8exo29YODUMuB0cONB0HRcqyTJb'\
-            'qnVPVaVbmxhoI_sybR76ACK-cldp7pg_8vcW5Zb64jI6JTlSOyPBr2v7BXqJ5TciXrp'\
-            '2E9IOvb8ZONsmWe-53Pg_Fmeeb29Fv7m5PDX2fJq7R19BR-tbm6dbgv-_kiwAoCEo7B'\
-            'Vkogz9YdBZqOD-8VpNaw1O8ikba9EYSvVbkNDnHPMnOO-ccwPjnlybuvDzr8eW-1elo'\
-            'Co8-tNknpdlA0gGgB2fgM1&cbcxt=&username=testo%40mduresources.com&mkt'\
-            '=&lc=')
-        
+    def __call__(self,username,password):
+
         # post data
         data = {
             'UserName':username,
@@ -43,7 +32,7 @@ class ADFS():
         
             # make the request
             resp = requests.post(
-                self.url+f'?{query}',
+                self.url,
                 data=data,
                 headers=self.headers,
                 verify=self.verify_ssl,
