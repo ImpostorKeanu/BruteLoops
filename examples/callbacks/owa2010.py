080f17ed201902e5a8cf8fa1a055cfda27f91099
Justin Angel <justinangel86@gmail.com>
2020-11-12 16:20:04 -0500

    adding owa2010.py

---
+++ examples/callbacks/owa2010.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import warnings
+warnings.filterwarnings('ignore')
+from re import search
+import requests
+
+class OWA2010:
+
+    def __init__(self, url, flags=0,
+            forcedownlevel=0,trusted=0, isUtf8=1,
+            proxies={}, headers={}, verify_ssl=False):
+
+        self.url = url
+        self.flags = flags
+        self.forcedownlevel = forcedownlevel
+        self.trusted = trusted
+        self.isUtf8 = isUtf8
+        self.proxies = proxies
+        self.headers = headers
+        self.verify_ssl = verify_ssl
+
+    def __call__(self,username,password,*args,**kwargs):
+    
+        # post data
+        data = {
+            'destination':url,
+            'flags':self.flags,
+            'forcedownlevel':self.forcedownlevel,
+            'trusted':self.trusted,
+            'username':username,
+            'password':password,
+            'isUtf8':self.isUtf8
+        }
+    
+        # make the request
+        resp = requests.post(self.url,
+                data=data,
+                headers=self.headers,
+                verify=self.verify_ssl,
+                allow_redirects=False,
+                proxies=self.proxies)
+    
+        # verify credentials and return outcome
+        if resp.status_code == 302 and resp.headers['Location'] and (
+            search(r'auth\/logon\.aspx\?', resp.headers['Location'])):
+            return [0, username, password]
+        else:
+            return [1, username, password]
