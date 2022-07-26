a35ec8f52c041b18f9dd2f037ac228847696952d
Justin Angel <justin@blackhillsinfosec.com>
2022-07-26 16:54:54 -0400

    stfu

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='bruteloops',
-    version='0.5.3',
+    version='0.5.4',
     author='Justin Angel',
     author_email='justin@arch4ngel.ninja',
     description='A simple password guessing API.',
